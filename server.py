0fe3a271235b0ae6b05c2f904bfb7907d8805fe9
ZhiYuan <s46129@yahoo.com.tw>
2025-03-13 08:55:51 +0800

    feat : Implement post

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Dict, Any
 
 app = FastAPI()
+
+class MessageStruct(BaseModel):
+    message: str
 
 @app.get("/test")
 async def test():
     return {"message": "OK!"}
+
+@app.post("/testPostMessageStruct")
+async def testPostMessageStruct(message:MessageStruct):
+    return {"message": "Received: " + message.message}
+
+@app.post("/testPost")
+async def testPost(data: Dict[str, Any]):
+    return {"received_data": data}
 
 if __name__ == "__main__":
     import uvicorn
